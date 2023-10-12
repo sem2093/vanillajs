e15ba397d9eaa7bb77749d1f8aae7a527a0d9b02
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-12 04:37:10 -0600

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,2 +1,20 @@
+//for statement
+let str = '';
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+console.log(str);
 
+//do...while
+
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+
+console.log(result);
+// Expected output: "12345"
  
