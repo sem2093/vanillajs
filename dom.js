172b28d224dce5dcdeb438e419fb0ba62f72d758
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-12 01:50:44 -0600

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,1 +1,19 @@
+//innerHTML,getELementById
+var time = document.getElementById("clock"); <br>
+  time.innerHTML = new Date()
 
+//getElementsByCLassName
+function toggle(checked) {
+if (check(this).checked = True) { document.getElementsByClassName("lang").checked = True; 
+  } else { document.getElementsByClassName("lang").checked = False;} 
+}
+
+
+
+
+
+//set Attribute
+var button = document.getElementById("btn");
+function on(){button.innerHTML="turn off"; button.setAttribute('onclick','off()');}
+var button = document.getElementById("btn");
+function off(){button.innerHTML="turn on"; button.setAttribute('onclick','on()');}
