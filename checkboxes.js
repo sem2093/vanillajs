3084a471e9ca6ae9199de65b339dc4ab4dd4f38b
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-20 06:27:02 -0600

    Update and rename checkboxes.js to json.html

--- checkboxes.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('#submit');
-        btn.addEventListener('click', (event) => {
-            let checkboxes = document.querySelectorAll('input[name="framework"]:checked');
-            let values = [];
-            checkboxes.forEach((checkbox) => {
-                values.push(checkbox.value);
-            });
-            alert(values);
-        });    
